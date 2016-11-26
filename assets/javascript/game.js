fc68bd74dd48f76c6291325ef6df715402530a27
Loretta Agyemang <Loretta Agyemang>
2016-11-26 17:03:19 -0500

    js image tag for numerical association

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//Sailor Moon's Magic Number
+/*The objective of this game is to match the magic number.
+Magic Number: 77 
+There are 4 Sailor Moon characters (Moon, Murcury, Venus, Mars) 
+Each is associated with a number. 
+Sailor Moon: 7 ; Sailor Mercury: 30; Sailor Venus: 10; Sailor Mars: 20 */
+
+//Associate image with number: JS or JQ? JS: http://stackoverflow.com/questions/32185692/how-to-assign-a-number-to-an-image-in-html 
