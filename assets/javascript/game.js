45b7821eece4b4430190b48c51c7c7bbf4f3ef38
Loretta Agyemang <Loretta Agyemang>
2016-11-28 14:09:15 -0500

    moving along slowly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,35 @@
 //Sailor Moon's Magic Number
 /*The objective of this game is to match the magic number.
-Magic Number: 77 
+Magic Number: Randomly generated numbers between (27-77)
+Click on the wand to generate a new number  
 There are 4 Sailor Moon characters (Moon, Murcury, Venus, Mars) 
-Each is associated with a number. 
-Sailor Moon: 7 ; Sailor Mercury: 30; Sailor Venus: 10; Sailor Mars: 20 */
+Each is associated with a randomly generated number. 
+First Round->Sailor Moon: 7 ; Sailor Mercury: 30; Sailor Venus: 10; Sailor Mars: 20 
+1. click on wand to start game and generate new random number
+2. click on characters to chose a number
+3. numbers appear in designated section*/
 
-//Associate image with number: JS or JQ? JS: http://stackoverflow.com/questions/32185692/how-to-assign-a-number-to-an-image-in-html 
+//Prepare for take off; Generate Random number (btw 27-77) after clicking wand image 
+$(document).ready(function(){
+
+})
+
+//Associate image with number-done in html
+
+/*var SailorMoon = 7;
+var SailorMercury = 30;
+var SailorVenus = 10;
+var SailorMars = 20; */ 
+
+Wand = Math.floor((Math.random() * 77) + 27; 
+console.log(Wand); 
+
+
+/*$('.Wand').on('click', function(){
+	Math.floor((Math.random() * 77) + 27)	
+}); */
+
+
+
+
+ 
