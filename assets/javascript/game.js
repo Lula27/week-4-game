86785b2ca86ef57a93bce75b92c05f9ef3606221
Loretta Agyemang <Loretta Agyemang>
2016-11-28 15:28:33 -0500

    progress with jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,10 +9,22 @@ First Round->Sailor Moon: 7 ; Sailor Mercury: 30; Sailor Venus: 10; Sailor Mars:
 2. click on characters to chose a number
 3. numbers appear in designated section*/
 
-//Prepare for take off; Generate Random number (btw 27-77) after clicking wand image 
-$(document).ready(function(){
+//Prepare for take off; Generate a random Magic number (btw 27-77) after clicking wand image 
+$(document).ready(function() {
 
-})
+	//When wand is clicked:
+	$(".Wand").on("click", function() {
+		var MagicNumber = ""; 
+
+		for (var i = 0; i < 1; i++) {
+		MagicNumber = MagicNumber + Math.floor((Math.random() * 9) + 1);
+		
+		}
+
+		$("#MagicNumber").prepend(MagicNumber + "<br>"); 
+	}); 
+
+}); 
 
 //Associate image with number-done in html
 
@@ -21,13 +33,6 @@ var SailorMercury = 30;
 var SailorVenus = 10;
 var SailorMars = 20; */ 
 
-Wand = Math.floor((Math.random() * 77) + 27; 
-console.log(Wand); 
-
-
-/*$('.Wand').on('click', function(){
-	Math.floor((Math.random() * 77) + 27)	
-}); */
 
 
 
