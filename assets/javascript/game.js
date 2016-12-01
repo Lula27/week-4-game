dce81414cb1ac709c737e8ae912fbcc2d78446ff
Loretta Agyemang <Loretta Agyemang>
2016-12-01 13:48:49 -0500

    got numbers to show up for Sailor Moon characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-
-
 //Sailor Moon's Magic Number
 /*The objective of this game is to match the magic number.
 Magic Number: Randomly generated numbers between (27-77)
@@ -11,8 +9,14 @@ First Round->Sailor Moon: 7 ; Sailor Mercury: 30; Sailor Venus: 10; Sailor Mars:
 2. click on characters to chose a number
 3. numbers appear in designated section*/
 
+//Generate variables
+var Wins = 0;
+var Losses = 0; 
 
-//Prepare for take off; Generate a random Magic number (btw 27-77) after clicking wand image 
+//Instructions
+// alert("Instructions: 1.Click on Wand  2.Pick Sailor Moon and her friends 3.Match the Magic Number!"); 
+
+//Prepare for take off; Generate a random Magic number (btw 19 - 120) after clicking wand image 
 $(document).ready(function() {
 
 	//When wand is clicked: 
@@ -27,7 +31,23 @@ $(document).ready(function() {
 		$("#MNumber").prepend(MagicNumber + "<br>"); 
 
 	}); 
+		//Fade numbers after click to make new numbers appear
+		// $("#MNumber").fadeOut(function() {
+		// 	$(this).text(MagicNumber).fadeIn();
+			
+		// });
+	//Generate random number with click on Sailor Moon character 
+	$(".SM,.SMer,.SV,.SMars").on("click", function() {
+		var MagicNumber = ""; 
 
+		for (var i = 0; i < 2; i++) {
+		MagicNumber = MagicNumber + Math.floor((Math.random() * 9) + 1);
+		
+		}
+
+		$("#MOutput").prepend(MagicNumber + "<br>"); 
+
+	}); 
 }); 
 
 
@@ -42,7 +62,7 @@ var SailorMercury = 30;
 var SailorVenus = 10;
 var SailorMars = 20; */ 
 
-// Retrieve number with click of character image
+// Retrieve number with click of character image (1 - 12)
 
 function Sailor(moon, mercury, venus, mars) {
 	this.name = name; 
