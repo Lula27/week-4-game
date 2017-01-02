7e8fce6e76161b2910d2ca245850836f780ae0ab
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-01-02 12:58:26 -0500

    image click to show value on page

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -7,7 +7,7 @@ var wins = 0;
 var losses = 0; 
 
 // Scores 
-var magicNumber = 0;
+var magicNumber = 7;
 var mNumber = 0;
 
 // Values for individual Sailor Scouts 
@@ -20,12 +20,41 @@ var mars = 1;
 $(document).ready(function() {
 
 // Add pointers for all images
-$(".images").css("cursor", "pointer"); 
+$('.images').css('cursor', 'pointer'); 
 
-console.log(venus); 
+// console.log(venus); 
+
+mNumber = moon + murcury + venus + mars;
+
+// console.log(mNumber);
+
+// Make numbers appear 
+$('#wand').on('click', function() {
+	$('#MNumber').html(magicNumber); 
+
+});
+
+$('.sM').on('click', function() {
+	$('#MOutput').html(moon); 
+});
+
+$('.sMer').on('click', function() {
+	$('#MOutput').html(moon); 
+});
+
+$('.sV').on('click', function() {
+	$('#MOutput').html(moon); 
+});
+
+$('.sMars').on('click', function() {
+	$('#MOutput').html(moon); 
+});
 
 
-
+// Wins & Losses
+// if(magicNumber === mNumber) {
+// 	alert("Winner"); 
+// }
 
 
 
